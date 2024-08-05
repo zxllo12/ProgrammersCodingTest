7817a95bf3908e9a337956197d5f7a7602bd02c8
zxllo12 <poapog123@naver.com>
2024-08-05 17:14:34 +0900

    문제120807

---
+++ Q_120807.cs
@@ -0,0 +1,29 @@
+﻿namespace ProgrammersCodingTest
+{
+    public class Q_120807
+    {
+        // 일자 : 2024-08-05
+        // 문제 : 정수 num1과 num2가 매개변수로 주어집니다. 두 수가 같으면 1 다르면 -1을 retrun하도록 solution 함수를 완성해주세요.
+
+        static void Main(string[] args)
+        {
+            int solution(int num1, int num2)
+            {
+                int answer = 0;
+
+                if (num1 == num2)
+                {
+                    answer = 1;
+                }
+                else
+                {
+                    answer = -1;
+                }
+
+                return answer;
+            }
+            
+            Console.WriteLine(solution(1, 2));
+        }
+    }
+}
