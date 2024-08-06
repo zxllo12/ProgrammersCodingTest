f5cbe2bbba303799ca978b6af3fc253e77b38354
zxllo12 <poapog123@naver.com>
2024-08-06 16:56:49 +0900

    주석오류수정

--- Q_120820.cs
+++ Q_120820.cs
@@ -9,8 +9,10 @@ namespace ProgrammersCodingTest
     public class Q_120820
     {
         // 일자 : 2024-08-06
-        // 문제 : 머쓱이는 선생님이 몇 년도에 태어났는지 궁금해졌습니다.
-        // 2022년 기준 선생님의 나이 age가 주어질 때, 선생님의 출생 연도를 return 하는 solution 함수를 완성해주세요
+        // 문제 : 두 수의 곱
+        // 문제 설명 : 머쓱이는 선생님이 몇 년도에 태어났는지 궁금해졌습니다.
+        //            2022년 기준 선생님의 나이 age가 주어질 때,
+        //            선생님의 출생 연도를 return 하는 solution 함수를 완성해주세요
 
         static void Main(string[] args)
         {
