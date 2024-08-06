29a8b3143146ed7bd53a37e8dd43d691ff504e43
zxllo12 <poapog123@naver.com>
2024-08-06 17:19:58 +0900

    문제120810

---
+++ Q_120810.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersCodingTest
+{
+    public class Q_120810
+    {
+        // 일자 : 2024-08-06
+        // 문제 : 나머지 구하기
+        // 문제 설명 : 정수 num1, num2가 매개변수로 주어질 때,
+        //            num1를 num2로 나눈 나머지를 return 하도록 solution 함수를 완성해주세요.
+
+        static void Main1(string[] args)
+        {
+            int solution(int num1, int num2)
+            {
+                int answer = num1 % num2;             
+
+                return answer;
+            }
+
+            Console.WriteLine(solution(3, 2));
+        }
+    }
+}
--- Q_120820.cs
+++ Q_120820.cs
@@ -9,12 +9,12 @@ namespace ProgrammersCodingTest
     public class Q_120820
     {
         // 일자 : 2024-08-06
-        // 문제 : 두 수의 곱
+        // 문제 : 나이 출력
         // 문제 설명 : 머쓱이는 선생님이 몇 년도에 태어났는지 궁금해졌습니다.
         //            2022년 기준 선생님의 나이 age가 주어질 때,
         //            선생님의 출생 연도를 return 하는 solution 함수를 완성해주세요
 
-        static void Main(string[] args)
+        static void Main1(string[] args)
         {
             int solution(int age)
             {
