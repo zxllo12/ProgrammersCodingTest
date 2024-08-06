13c94b5f6ed5c0797470f2b8c8b977cdcb0e98d5
zxllo12 <poapog123@naver.com>
2024-08-06 16:53:22 +0900

    메인막음

--- Q_120807.cs
+++ Q_120807.cs
@@ -5,7 +5,7 @@
         // 일자 : 2024-08-05
         // 문제 : 정수 num1과 num2가 매개변수로 주어집니다. 두 수가 같으면 1 다르면 -1을 retrun하도록 solution 함수를 완성해주세요.
 
-        static void Main(string[] args)
+        static void Main1(string[] args)
         {
             int solution(int num1, int num2)
             {
