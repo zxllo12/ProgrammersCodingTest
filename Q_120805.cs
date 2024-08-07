02a0a51dc05fd83c133da73ec7b888faace64061
zxllo12 <poapog123@naver.com>
2024-08-07 17:54:58 +0900

    문제 120805

---
+++ Q_120805.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersCodingTest
+{
+    public class Q_120805
+    {
+        // 일자 : 2024-08-07
+        // 문제 : 몫 구하기
+        // 문제 설명 : 정수 num1, num2가 매개변수로 주어질 때,
+        //            num1을 num2로 나눈 몫을 return 하도록 solution 함수를 완성해주세요.
+
+        static void Main1(string[] args)
+        {
+            int solution(int num1, int num2)
+            {
+                int answer = num1 / num2;             
+
+                return answer;
+            }
+
+            Console.WriteLine(solution(3, 2));
+        }
+    }
+}
